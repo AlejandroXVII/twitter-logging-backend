a279e12f1da78b2d84c1410dc5aef56cbbe17a05
AlejandroXVII <m.alejandro.xvii@gmail.com>
2024-03-14 00:45:07 -0400

    Update user model

--- models/user.js
+++ models/user.js
@@ -3,14 +3,39 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	full_name: { type: String, required: true },
+	first_name: { type: String, required: true },
+	last_name: { type: String, required: true },
 	username: { type: String, required: true },
 	email: { type: String, required: true },
+	location: { type: String, required: false },
+	birthday: { type: Date, required: true },
 	password: { type: String, required: true },
 	chats: [
 		{
 			user_id: { type: Schema.Types.ObjectId, ref: "User" },
 			chat_id: { type: Schema.Types.ObjectId, ref: "Chat" },
+		},
+	],
+	tweets: [
+		{
+			user_id: { type: Schema.Types.ObjectId, ref: "User" },
+			tweet_id: { type: Schema.Types.ObjectId, ref: "Tweet" },
+		},
+	],
+	following: [
+		{
+			user_id: { type: Schema.Types.ObjectId, ref: "User" },
+		},
+	],
+	followers: [
+		{
+			user_id: { type: Schema.Types.ObjectId, ref: "User" },
+		},
+	],
+	likes: [
+		{
+			user_id: { type: Schema.Types.ObjectId, ref: "User" },
+			tweet_id: { type: Schema.Types.ObjectId, ref: "Tweet" },
 		},
 	],
 });
